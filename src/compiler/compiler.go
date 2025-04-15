64c33e24240e220223bda4a1ae696f99698a64b3
Pedro Giroldo <pedrogiroldo40@gmail.com>
2025-04-15 09:54:22 -0300

    Refatorar a lógica de compilação para o pacote compiler

---
+++ src/compiler/compiler.go
@@ -0,0 +1,39 @@
+package compiler
+
+import (
+	"CExec/src/fileReader"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func Compile(
+	config fileReader.ConfigFile,
+	arquivo string,
+	output string,
+) {
+	// Define o comando do compilador e argumentos
+	var compilerCmd string
+	if config.CompilerPath != "" {
+		compilerCmd = config.CompilerPath
+	} else {
+		fmt.Printf("Compilador não especificado.")
+		os.Exit(1)
+
+	}
+
+	// Prepara os argumentos de compilação
+	compilerArgs := []string{"-o", output, arquivo}
+	if len(config.CompilerArgs) > 0 {
+		compilerArgs = append([]string{"-o", output}, config.CompilerArgs...)
+		compilerArgs = append(compilerArgs, arquivo)
+	}
+
+	// Executa a compilação
+	compilacao := exec.Command(compilerCmd, compilerArgs...)
+	compilacao.Stderr = os.Stderr
+	if err := compilacao.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Erro na compilação: %v\n", err)
+		os.Exit(1)
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"CExec/src/compiler"
 	"CExec/src/fileReader"
 	"fmt"
 	"os"
@@ -34,30 +35,7 @@ func main() {
 		output = "output.exe"
 	}
 
-	// Define o comando do compilador e argumentos
-	var compilerCmd string
-	if config.CompilerPath != "" {
-		compilerCmd = config.CompilerPath
-	} else {
-		fmt.Printf("Compilador não especificado.")
-		os.Exit(1)
-
-	}
-
-	// Prepara os argumentos de compilação
-	compilerArgs := []string{"-o", output, arquivo}
-	if len(config.CompilerArgs) > 0 {
-		compilerArgs = append([]string{"-o", output}, config.CompilerArgs...)
-		compilerArgs = append(compilerArgs, arquivo)
-	}
-
-	// Executa a compilação
-	compilacao := exec.Command(compilerCmd, compilerArgs...)
-	compilacao.Stderr = os.Stderr
-	if err := compilacao.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Erro na compilação: %v\n", err)
-		os.Exit(1)
-	}
+	compiler.Compile(config, arquivo, output)
 
 	// Executa o programa compilado se configurado para isso
 	if config.RunAfterCompile {
